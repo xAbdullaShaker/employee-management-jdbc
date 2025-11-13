6d45a87291c1977f92dbfdc593068f0f96b5de17
Abdulla Shaker <abdullashaker002@gmail.com>
2025-11-13 11:08:13 +0300

    Update DBConnection.java

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
     // TODO: Change credentials to your real Postgres settings when ready.
     private static final String URL = "jdbc:postgresql://localhost:5432/ems_db";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "CHANGE_ME";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
